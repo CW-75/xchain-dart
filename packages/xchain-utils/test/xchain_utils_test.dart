56f0046c2edaf9623c5baf0c02ae7e7f304304cd
CW-75 <cw75dev@gmail.com>
2025-09-17 02:15:20 -0400

    added asset format tests

--- packages/xchain-utils/test/xchain_utils_test.dart
+++ packages/xchain-utils/test/xchain_utils_test.dart
@@ -2,6 +2,34 @@ import 'package:test/test.dart';
 import 'package:xchain_utils/xchain_utils.dart';
 
 void main() {
+  final Asset testNativeAsset = (
+    chain: 'ETH',
+    symbol: 'ETH',
+    ticker: 'ETH',
+    type: AssetType.native,
+  );
+
+  final Asset testTokenAsset = (
+    chain: 'ETH',
+    symbol: 'USDT-0x1234567890abcdef',
+    ticker: 'USDT',
+    type: AssetType.token,
+  );
+
+  final Asset testTradeAsset = (
+    chain: 'ETH',
+    symbol: 'USDT-0x1234567890abcdef',
+    ticker: 'USDT',
+    type: AssetType.trade,
+  );
+
+  final Asset testSecuredAsset = (
+    chain: 'ETH',
+    symbol: 'USDT-0x1234567890abcdef',
+    ticker: 'USDT',
+    type: AssetType.secured,
+  );
+
   group('Asset Test group', () {
     setUp(() {
       // Additional setup goes here.
@@ -36,6 +64,23 @@ void main() {
       expect(asset.ticker, 'USDT');
       expect(asset.chain, 'ETH');
       expect(asset.type, AssetType.secured);
+    });
+
+    test('Asset Native to String ', () {
+      String assetStr = assetToString(testNativeAsset);
+      expect(assetStr, 'ETH.ETH');
+    });
+    test('Asset Token to String ', () {
+      String assetStr = assetToString(testTokenAsset);
+      expect(assetStr, 'ETH.USDT-0x1234567890abcdef');
+    });
+    test('Asset Trade to String ', () {
+      String assetStr = assetToString(testTradeAsset);
+      expect(assetStr, 'ETH~USDT-0x1234567890abcdef');
+    });
+    test('Asset Secured to String ', () {
+      String assetStr = assetToString(testSecuredAsset);
+      expect(assetStr, 'ETH-USDT-0x1234567890abcdef');
     });
   });
 }
