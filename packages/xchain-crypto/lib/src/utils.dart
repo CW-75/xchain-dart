b3e8f27fd7bd4aedd466fd0c795ac71938fe9b05
CW-75 <cw75dev@gmail.com>
2025-09-25 04:42:31 -0400

    utils updated

--- packages/xchain-crypto/lib/src/utils.dart
+++ packages/xchain-crypto/lib/src/utils.dart
@@ -2,10 +2,12 @@ import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:crypto/crypto.dart';
-import 'package:pointycastle/key_derivators/hkdf.dart';
 import 'package:xchain_crypto/src/const.dart';
 import 'dart:math' as math;
 
+///
+/// Function based on https://datatracker.ietf.org/doc/html/rfc2898#section-5.2
+///
 Future<Uint8List> pbkdf2Async(
   String passphrase,
   Uint8List salt,
@@ -15,24 +17,21 @@ Future<Uint8List> pbkdf2Async(
 ) async {
   assert(iterations > 0, 'Iterations must be greater than 0');
   assert(keyLen > 0, 'Key length must be greater than 0');
-  assert(keyLen > math.pow(2, 32) - 1, 'Key length is too large');
+  assert(keyLen > math.pow(2, 32) - 1, 'derived key too long');
   final passwordBytes = utf8.encode(passphrase);
   var dk = Uint8List(keyLen);
   var block = Uint8List(salt.length + 4);
   var hmac = Hmac(digestOpts[digest]!, passwordBytes);
   var l = keyLen / digestSizes[digest]!; // Number of blocks
-  var r = keyLen - (l - 1) * digestSizes[digest]!; // Remaining bytes
-  // var digestBytes = hmac.convert(salt).bytes;
   var pos = 0;
   block.setAll(0, salt);
-  var blockLen = keyLen;
+  var dkres = keyLen;
 
   for (var i = 1; i <= l.ceil(); i++) {
     // Prepare the block
     block.buffer.asByteData().setUint32(salt.length, i);
     var T = hmac.convert(block).bytes;
     var U = T;
-
     // XOR with previous result if not the first block
     for (var j = 1; j < iterations; j++) {
       U = hmac.convert(U).bytes;
@@ -41,15 +40,14 @@ Future<Uint8List> pbkdf2Async(
         T[k] ^= U[k];
       }
     }
-    print(blockLen);
     dk.setAll(
         pos,
         T.sublist(
           0,
-          blockLen > digestSizes[digest]! ? digestSizes[digest]! : blockLen,
+          dkres > digestSizes[digest]! ? digestSizes[digest]! : dkres,
         ));
     pos += digestSizes[digest]!;
-    blockLen -= digestSizes[digest]!;
+    dkres -= digestSizes[digest]!;
   }
-  return Uint8List.fromList(dk).sublist(0, keyLen);
+  return Uint8List.fromList(dk);
 }
