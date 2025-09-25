feb135486d44a5c09ff1ba70a74df6df262327a7
CW-75 <cw75dev@gmail.com>
2025-09-25 03:40:04 -0400

    able utils to work with sha256

--- packages/xchain-crypto/lib/src/utils.dart
+++ packages/xchain-crypto/lib/src/utils.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:pointycastle/export.dart';
+import 'package:crypto/crypto.dart';
+import 'package:pointycastle/key_derivators/hkdf.dart';
 import 'package:xchain_crypto/src/const.dart';
 
 Future<Uint8List> pbkdf2Async(
@@ -9,14 +10,38 @@ Future<Uint8List> pbkdf2Async(
   Uint8List salt,
   int iterations,
   int keyLen,
-  String digest,
+  DigestAlgorithm digest,
 ) async {
-  var pbkdf2 = PBKDF2KeyDerivator(
-    HMac(SHA256Digest(), hmac_size), // SHA-256 digest
-  );
-  final params = Pbkdf2Parameters(salt, iterations, keyLen);
-  pbkdf2.init(params);
+  assert(iterations > 0, 'Iterations must be greater than 0');
+  assert(keyLen > 0, 'Key length must be greater than 0');
+
   final passwordBytes = utf8.encode(passphrase);
-  final derivedKey = pbkdf2.process(passwordBytes);
-  return derivedKey;
+  var dk = Uint8List(keyLen);
+  var block = Uint8List(salt.length + 4);
+  var hmac = Hmac(digestOpts[digest]!, passwordBytes);
+  var l = digestSizes[digest]! ~/ keyLen; // Number of blocks
+  // var digestBytes = hmac.convert(salt).bytes;
+  print(hmac.toString());
+  block.setAll(0, salt);
+  var despos = 0;
+  for (var i = 1; i <= l; i++) {
+    // Prepare the block
+    block.buffer.asByteData().setUint32(salt.length, i);
+    // print('Block: ${block.map((e) => e.toRadixString(16)).toList()}');
+    // Compute the HMAC for the block
+    var T = hmac.convert(block).bytes;
+    var U = T;
+    print(T.length);
+
+    // XOR with previous result if not the first block
+    for (var j = 1; j < iterations; j++) {
+      U = hmac.convert(U).bytes;
+
+      for (var k = 0; k < digestSizes[digest]!; k++) {
+        T[k] ^= U[k];
+      }
+    }
+    dk.setAll(0, T);
+  }
+  return Uint8List.fromList(dk);
 }
