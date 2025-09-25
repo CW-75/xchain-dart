3cbc06e6e289a5e012b59885dc03ff6a011aa9cf
CW-75 <cw75dev@gmail.com>
2025-09-25 04:50:19 -0400

    fixed const

--- packages/xchain-crypto/lib/src/const.dart
+++ packages/xchain-crypto/lib/src/const.dart
@@ -5,7 +5,6 @@ const String kdf = 'pbkdf2'; // Key derivation function
 const String prf = 'hmac-sha256'; // Pseudorandom function
 const int dklen = 32; // Length of derived key in bytes
 const int c = 262144; // Iterations
-const int hmac_size = 64; // Size of HMAC output in bytes
 
 enum DigestAlgorithm {
   sha256,
