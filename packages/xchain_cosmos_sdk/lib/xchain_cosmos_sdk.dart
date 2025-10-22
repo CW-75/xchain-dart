25a36ff1d23ec1a94136ee3f8dd622420c7da6fb
CW-75 <cw75dev@gmail.com>
2025-10-22 00:03:22 -0400

    added client model getting balances

--- packages/xchain_cosmos_sdk/lib/xchain_cosmos_sdk.dart
+++ packages/xchain_cosmos_sdk/lib/xchain_cosmos_sdk.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library;
 
-export 'src/tendermint.dart';
+export 'src/client.dart';
 
 // TODO: Export any libraries intended for clients of this package.
--- packages/xchain_cosmos_sdk/test/xchain_cosmos_sdk_test.dart
+++ packages/xchain_cosmos_sdk/test/xchain_cosmos_sdk_test.dart
@@ -1,37 +1,122 @@
-import 'package:xchain_cosmos_sdk/src/tendermint/client.dart';
+import 'package:xchain_client/xchain_client.dart';
+import 'package:xchain_cosmos_sdk/src/client.dart';
+// import 'package:xchain_cosmos_sdk/src/tendermint/client.dart';
 import 'package:test/test.dart';
-import 'package:xchain_cosmos_sdk/src/tendermint/v1beta1/coin.dart';
-import 'package:xchain_cosmos_sdk/src/tendermint/v1beta1/query.dart';
+// import 'package:xchain_cosmos_sdk/src/tendermint/v1beta1/coin.dart';
+// import 'package:xchain_cosmos_sdk/src/tendermint/v1beta1/query.dart';
+import 'package:xchain_utils/xchain_utils.dart';
+
+final class PseudoCosmosClient extends CosmosSdkClient {
+  PseudoCosmosClient({
+    required Chain chain,
+    required Network network,
+    DataProviders? dataProviders,
+  }) : super(chain: chain, network: network, dataProviders: dataProviders);
+
+  @override
+  Future<TxHash> broadcastTx(String txHex) {
+    // TODO: implement broadcastTx
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Fees> getFees(FeeEstimateOption options) {
+    // TODO: implement getFees
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<PreparedTx> prepareTx(TxParams params) {
+    // TODO: implement prepareTx
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<TxHash> transfer(TxParams params) {
+    // TODO: implement transfer
+    throw UnimplementedError();
+  }
+
+  @override
+  bool validateAddress(Address address) {
+    // TODO: implement validateAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Address getAddress(int? walletIndex) {
+    // TODO: implement getAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  AssetInfo getAssetInfo(Asset asset) {
+    // TODO: implement getAssetInfo
+    throw UnimplementedError();
+  }
+
+  @override
+  String getExplorerTxUrl(TxHash txHash) {
+    // TODO: implement getExplorerTxUrl
+    throw UnimplementedError();
+  }
+
+  @override
+  String getExplorerAddressUrl(Address address) {
+    // TODO: implement getExplorerAddressUrl
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Address> getAsyncAddress(int? walletIndex) {
+    // TODO: implement getAsyncAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  String getExplorerUrl() {
+    // TODO: implement getExplorerUrl
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Tx> getTransaction(TxHash txHash) {
+    // TODO: implement getTransaction
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<TxsPage> getTransactions(TxHistoryParams params) {
+    // TODO: implement getTransactions
+    throw UnimplementedError();
+  }
+}
 
 void main() {
-  group('A group of tests', () {
+  group('Pseudo Cosmos SDKL client implementation test', () {
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () async {
-      TendermintClient client = TendermintClient.connect(
-        endpoint: 'https://cosmos-rpc.publicnode.com',
-        // endpoint: 'https://rpc.ninerealms.com',
+    test('Create Client and get balances from rpc node', () async {
+      PseudoCosmosClient client = PseudoCosmosClient(
+        chain: 'THOR',
+        network: Network.mainnet,
+        dataProviders: {
+          // Network.mainnet: ['https://cosmos-rpc.publicnode.com'],
+          Network.mainnet: ['https://rpc.ninerealms.com'],
+        },
       );
-      var address = 'cosmos1meq6c9m3qyc4vfepgcrx4suyxsa3ec4c0haxv7';
-      // var address = 'thor1n7rqhtw9aek3e6j84ee9xjnxz729n0nvk86p0r';
-      // print(hexlify(Uint8List.fromList(address.codeUnits)));
-      // print('Address: ${Uint32List.fromList(address.codeUnits).lengthInBytes}');
-      QueryAllBalanceRequest request = QueryAllBalanceRequest(address: address);
 
-      // print('${writer.finish()}');
+      final balances = await client.getBalance(
+        // address: 'cosmos1meq6c9m3qyc4vfepgcrx4suyxsa3ec4c0haxv7',
+        address: 'thor1n7rqhtw9aek3e6j84ee9xjnxz729n0nvk86p0r',
+      );
 
-      var info = await client.abciQuerry(
-        path: '/cosmos.bank.v1beta1.Query/AllBalances',
-        data: request.encode(),
-      );
-      QueryAllBalanceResponse response = QueryAllBalanceResponse(
-        buff: info.value!,
-      );
-      List<Coin> coins = response.decode();
-      for (var coin in coins) {
-        print('Coin: ${coin.denom}, Amount: ${coin.amount}');
+      for (var balance in balances) {
+        print(
+          'Asset: ${assetToString(balance.asset)}, Amount: ${baseToAsset(balance.amount).toString()}',
+        );
       }
     });
   });
